18975d77a25698513d356a51fa511e7c0c9401e5
Aydin Maleki <2148169@talnet.nl>
2021-10-15 14:35:22 +0200

    eind

--- game.py
+++ game.py
@@ -1,5 +1,25 @@
 print("Hi")
 name = input ("What is your name?")
 
-print("It's nice to meet you,", name)
+print("It's nice to meet you,",name)
+
+som = input ("Do you want to play a game?")
+
+if som == "yes":
+    num1 = input ("Please insert the fisrt number: ")
+    num2 = input ("Please insert the second number: ")
+    num1= int(num1)
+    num2= int(num2)
+    operation = input ("Please insert your desired operation among: + - * / :")
+    if operation == "+":
+        result = num1 + num2
+    if operation == "-":
+        result = num1 - num2
+    if operation == "*":
+        result = num1 * num2
+    if operation == "/":
+        result = num1 / num2
+    print(result)  
+else:
+    print("Okay, have a nice day")
 
